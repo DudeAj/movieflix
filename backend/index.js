18dbb1463d8d84da1bf0ed4d18ecae92b18a7ea9
Ajay gangwar <gangwar.ajay@orangemantra.in>
2023-05-18 10:07:07 +0530

    code

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const cors = require("cors");
-const AppError = require("./utils/AppError")
-const UserRoutes = require("./routes/user");
-const WatchlistRoutes = require("./routes/watchlist")
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const AppError = require('./utils/AppError');
+const UserRoutes = require('./routes/user');
+const WatchlistRoutes = require('./routes/watchlist');
 const app = express();
 require('dotenv').config();
 
@@ -14,32 +14,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
 
 app.use('/api/watch', WatchlistRoutes);
-app.use("/api/user", UserRoutes);
+app.use('/api/user', UserRoutes);
 
 mongoose.connect(process.env.MONGO_URL, (err, res) => {
-    if (err) throw err;
-
+  if (err) throw err;
+  console.log('connected to server');
 });
-
 
 app.get('/', (req, res) => {
-    res.send("how are you doing today");
+  res.send('how are you doing today');
 });
 
-app.all("*", (req, res, next) => {
-    const err = new AppError(`Requested URL ${req.path} not found!`, 404);
-    next(err);
+app.all('*', (req, res, next) => {
+  const err = new AppError(`Requested URL ${req.path} not found!`, 404);
+  next(err);
 });
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    res.json({
-        statusCode: statusCode,
-        success: 0,
-        message: err.message
-    })
-})
-
-app.listen(PORT, () => {
-
+  const statusCode = err.statusCode || 500;
+  res.json({
+    statusCode: statusCode,
+    success: 0,
+    message: err.message,
+  });
 });
+
+app.listen(PORT, () => {});
